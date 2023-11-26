5e42396e7435f848559c269a55e1ea4652a54726
itsbamee <jan9239@naver.com>
2023-11-26 11:19:39 +0900

    쿠키 팝업 기능 개선

--- js/main.js
+++ js/main.js
@@ -1,37 +1,55 @@
-const [open, view, del] = document.querySelectorAll('nav button');
-const popup = document.querySelector('aside');
-const ck = popup.querySelector('#ck');
-const close = popup.querySelector('button');
+const [view, del] = document.querySelectorAll('nav button');
+const lastTime = 2;
 
-document.cookie.indexOf('today=done') < 0
-	? (popup.style.display = 'block')
-	: (popup.style.display = 'none');
+//1. 다큐먼트 쿠키가 있을 때에만 보여주고
+if (document.cookie.indexOf('today=done') < 0) {
+	createPop({ wid: 600, bg: 'lightcoral', lastTime });
+}
 
-//쿠키확인
+//쿠키만 확인
 view.addEventListener('click', () => console.log(document.cookie));
 
+//쿠키만 삭제
 del.addEventListener('click', () => {
-	setCookie('today', 'done', 24);
-	//만료시간이 오늘이 되므로 바로 쿠키가 삭제됨
+	setCookie('today', 'done', 0);
 	alert('쿠키삭제');
 });
 
-//팝업 열기
-open.addEventListener('click', () => {
-	popup.style.display = 'block';
-});
-
-//팝업 닫기
-close.addEventListener('click', () => {
-	if (ck.checked) setCookie('today', 'done', 1);
-	popup.style.display = 'none';
-});
-
 //쿠키생성 함수
-function setCookie(name, value, expires) {
+function setCookie(name, value, hours) {
 	let now = new Date();
 	//현재 밀리세컨드 시간값에 1시간(1000*60*60) * expires
-	let dueDate = now.getTime() + 1000 * 60 * 60 * expires; //밀리세컨드 변환
-	now.setTime(dueDate); //10초 추가된 값으로 setTime바꿈
+	let dueDate = now.getTime() + 1000 * 60 * 60 * hours; //밀리세컨드 변환
+	now.setTime(dueDate);
 	document.cookie = `${name}=${value}; path=/; expires=${now.toUTCString()}`;
+}
+
+//팝업생성 함수 (쿠키가 있으면 다 만들어지고)
+function createPop({ wid = 400, bg = '#ddd', lastTime = 24 }) {
+	const aside = document.createElement('aside');
+	aside.style.width = wid + 'px';
+	aside.style.backgroundColor = bg;
+
+	const tags = `
+  <div class="con"></div>
+  <div class="controls">
+    <p>
+      <input type="checkbox" id="ck">
+      <label for="ck">${lastTime}시간동안 보지 않기</label>
+    </p>
+    <button>CLOSE</button>
+  </div>
+  `;
+	aside.innerHTML = tags;
+	document.body.append(aside); //aside유지하면서 body안쪽에 들어감
+
+	//닫기버튼은 팝업이 있을때만 작동을 하므로
+	const close = document.querySelector('aside button');
+	const ck = document.querySelector('aside #ck');
+
+	//팝업 제거할때 쿠키가 없는 상태에서 닫히는거니까 새로고침하면 다시 쿠키가 만들어질거고
+	close.addEventListener('click', () => {
+		if (ck.checked) setCookie('today', 'done', lastTime);
+		document.querySelector('aside').remove();
+	});
 }
