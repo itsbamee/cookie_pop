ec284b1bfa991700a7e0cb68b8ab2e9c2fbef1f9
itsbamee <jan9239@naver.com>
2023-11-26 09:45:45 +0900

    팝업 닫기 시 체크박스 선택 시 쿠키 생성

--- js/main.js
+++ js/main.js
@@ -3,20 +3,24 @@ const popup = document.querySelector('aside');
 const ck = popup.querySelector('#ck');
 const close = popup.querySelector('button');
 
+//쿠키확인
+view.addEventListener('click', () => console.log(document.cookie));
+
+//팝업 열기
 open.addEventListener('click', () => {
 	popup.style.display = 'block';
 });
 
+//팝업 닫기
 close.addEventListener('click', () => {
+	if (ck.checked) setCookie('today', 'done', 1);
 	popup.style.display = 'none';
 });
 
+//쿠키생성 함수
 function setCookie(name, value, expires) {
-	let today = new Date(); //시작 인수값
+	let today = new Date();
 	let dueDate = today.getDate() + expires;
-	//현재 시간으로부터 2틀뒤로 덮어야 됨
-	//setDate
 	today.setDate(dueDate);
 	document.cookie = `${name}=${value}; path=/; expires=${today.toString()}`;
-	//today.toString() -> 숫자를 강제로 문자로 변경함
 }
