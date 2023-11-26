e4736f622056a7dd04309a97558ce3c630e3e333
itsbamee <jan9239@naver.com>
2023-11-26 09:35:43 +0900

    인수로 쿠키 정보와 만료시간을 받아서 해당 만료시간까지 유지되는 쿠키생성함수 추가

--- js/main.js
+++ js/main.js
@@ -1,10 +1,22 @@
-/*
-  cookie는 사용자의 컴퓨터에 물리적인 파일 형태로 남기는 경량의 텍스트 자료
-  name=value 형식으로 저장됨;
-  쿠키 생성시 쿠키의 생성주기를 설정가능
-  name=value; path=/; expires=만료시간;
+const [open, view, del] = document.querySelectorAll('nav button');
+const popup = document.querySelector('aside');
+const ck = popup.querySelector('#ck');
+const close = popup.querySelector('button');
 
-  문자열.indexOf(찾을 문자열) : 전체 문자열에서 해당 문자열의 위치순서값 반환
-  (indexof 쓰는이유 : 마이너스를 반환하는지 하지 않는지를 찾기 위함)
-  전체 문자열에서 찾을 문자열이 없으면 기본적으로 -1 반환
-*/
+open.addEventListener('click', () => {
+	popup.style.display = 'block';
+});
+
+close.addEventListener('click', () => {
+	popup.style.display = 'none';
+});
+
+function setCookie(name, value, expires) {
+	let today = new Date(); //시작 인수값
+	let dueDate = today.getDate() + expires;
+	//현재 시간으로부터 2틀뒤로 덮어야 됨
+	//setDate
+	today.setDate(dueDate);
+	document.cookie = `${name}=${value}; path=/; expires=${today.toString()}`;
+	//today.toString() -> 숫자를 강제로 문자로 변경함
+}
